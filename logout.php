50214ac47190dcb695c9fed4cf496779b989ad6c
Hannelorevb04 <hannelorevb@hotmail.be>
2024-04-21 18:43:09 +0200

    db

---
+++ logout.php
@@ -0,0 +1,5 @@
+<?php 
+    // setcookie("loggedin", "", time()-3600); //cookie weggooien, time-3600 is om de cookie terug in de tijd te nemen
+    session_start(); //server weet wie wij zijn, met welke data
+    session_destroy();
+    header('location: login.php');
