5ed461609eb7764cc120ba73f850903a4435019d
Hannelorevb04 <hannelorevb@hotmail.be>
2024-05-05 23:27:16 +0200

    tasks.sql

--- db/hub_tasks.sql
+++ db/hub_tasks.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Gegenereerd op: 05 mei 2024 om 19:13
+-- Gegenereerd op: 05 mei 2024 om 21:26
 -- Serverversie: 5.7.24
 -- PHP-versie: 8.0.1
 
@@ -31,11 +31,19 @@ CREATE TABLE `hub_tasks` (
   `id` int(11) NOT NULL,
   `task_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `task_description` text COLLATE utf8mb4_unicode_ci,
-  `task_type` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `task_type` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `task_status` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Open',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `hub_tasks`
+--
+
+INSERT INTO `hub_tasks` (`id`, `task_name`, `task_description`, `task_type`, `task_status`, `created_at`, `updated_at`) VALUES
+(1, 'cleaning', 'cleaning', 'cleaning', 'Open', '2024-05-05 19:38:11', '2024-05-05 19:38:11'),
+(6, 'hygiene', 'hygiene', NULL, 'Open', '2024-05-05 21:19:26', '2024-05-05 21:19:26');
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -55,7 +63,7 @@ ALTER TABLE `hub_tasks`
 -- AUTO_INCREMENT voor een tabel `hub_tasks`
 --
 ALTER TABLE `hub_tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
