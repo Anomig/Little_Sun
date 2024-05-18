0bcc9e8daed136ba1f8eb80b949aa543ba3e20d0
Hannelorevb04 <hannelorevb@hotmail.be>
2024-05-18 21:28:34 +0200

    calendar

--- calendar/display_task.php
+++ calendar/display_task.php
@@ -1,34 +1,46 @@
 <?php
-require 'db.php'; // Laad de databaseverbinding in
+require 'db.php'; // Zorg ervoor dat db.php in dezelfde directory staat
 
-$display_query = "SELECT id, task_name, task_start_date, task_end_date FROM hub_tasks";
-$results = Db::getConnection()->query($display_query); // Gebruik de PDO-verbinding van db.php
-$count = $results->rowCount();
+try {
+    // Query om taken op te halen
+    $display_query = "SELECT id, task_name, task_start_date, task_end_date FROM hub_tasks";
+    $results = Db::getConnection()->query($display_query);
 
-if ($count > 0) {
-    $data_arr = array();
-    $i = 1;
-    while ($data_row = $results->fetch(PDO::FETCH_ASSOC)) {
-        $data_arr[$i]['id'] = $data_row['id'];
-        $data_arr[$i]['title'] = $data_row['task_name'];
-        $data_arr[$i]['start'] = date("Y-m-d", strtotime($data_row['task_start_date']));
-        $data_arr[$i]['end'] = date("Y-m-d", strtotime($data_row['task_end_date']));
-        $data_arr[$i]['color'] = '#' . substr(uniqid(), -6); // 'green'; pass colour name
-        $data_arr[$i]['url'] = 'https://www.shinerweb.com';
-        $i++;
+    // Check of er resultaten zijn
+    if ($results->rowCount() > 0) {
+        $data_arr = array();
+        $i = 0;
+        while ($data_row = $results->fetch(PDO::FETCH_ASSOC)) {
+            $data_arr[$i]['id'] = $data_row['id'];
+            $data_arr[$i]['title'] = $data_row['task_name'];
+            $data_arr[$i]['start'] = $data_row['task_start_date'];
+            $data_arr[$i]['end'] = $data_row['task_end_date'];
+            $data_arr[$i]['color'] = '#' . substr(uniqid(), -6); // Voeg een willekeurige kleur toe
+            $data_arr[$i]['url'] = '#'; // Voeg eventueel een URL toe
+            $i++;
+        }
+
+        // Succesvolle respons
+        $data = array(
+            'status' => true,
+            'msg' => 'Tasks fetched successfully!',
+            'data' => $data_arr
+        );
+    } else {
+        // Geen taken gevonden
+        $data = array(
+            'status' => false,
+            'msg' => 'No tasks found.'
+        );
     }
-
-    $data = array(
-        'status' => true,
-        'msg' => 'successfully!',
-        'data' => $data_arr
-    );
-} else {
+} catch (PDOException $e) {
+    // Fout bij het uitvoeren van de query
     $data = array(
         'status' => false,
-        'msg' => 'Error!'
+        'msg' => 'Database error: ' . $e->getMessage()
     );
 }
-echo json_encode($data);
 
+// JSON-respons
+echo json_encode($data);
 ?>
